c09988c0a77a8afbae80557dc743a213b176536d
pranoppal <pranoppal1006@gmail.com>
2019-05-22 12:42:01 +0530

    Added CardView

--- app/src/main/java/com/example/newsheadlines/adapter/Mainarticleadapter.java
+++ app/src/main/java/com/example/newsheadlines/adapter/Mainarticleadapter.java
@@ -3,14 +3,17 @@ package com.example.newsheadlines.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.newsheadlines.Activity.MainActivity;
 import com.example.newsheadlines.Activity.WebActivity;
 import com.example.newsheadlines.R;
@@ -34,7 +37,7 @@ public class Mainarticleadapter extends RecyclerView.Adapter<Mainarticleadapter.
     @NonNull
     @Override
     public Mainarticleadapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.article_adapter,viewGroup,false);
+        View view= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_view,viewGroup,false);
         return new Mainarticleadapter.ViewHolder(view);
     }
 
@@ -42,7 +45,14 @@ public class Mainarticleadapter extends RecyclerView.Adapter<Mainarticleadapter.
     public void onBindViewHolder(@NonNull final Mainarticleadapter.ViewHolder viewHolder, int i) {
 
         viewHolder.titletext.setText(marticlelist.get(i).getTitle());
-        viewHolder.descriptiontext.setText(marticlelist.get(i).getDescription());
+        Glide.with(mcontext)
+                .load(marticlelist.get(i).getUrlToImage())
+                .thumbnail(0.1f)
+                .centerCrop()
+                .error(R.drawable.ic_launcher_background)
+                .into(viewHolder.imgview);
+        viewHolder.cardView.startLayoutAnimation();
+
     }
 
     @Override
@@ -54,12 +64,14 @@ public class Mainarticleadapter extends RecyclerView.Adapter<Mainarticleadapter.
 
     class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView titletext;
-        private TextView descriptiontext;
+        private ImageView imgview;
+        private CardView cardView;
 
         ViewHolder(View itemview) {
             super(itemview);
             titletext = itemview.findViewById(R.id.title);
-            descriptiontext = itemview.findViewById(R.id.description);
+            imgview=itemview.findViewById(R.id.img_card_main);
+            cardView=itemview.findViewById(R.id.card_row);
             itemview.setOnClickListener(this);
         }
         @Override
